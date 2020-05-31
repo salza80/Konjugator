e5bb058db137cb9675d7d56c3a6d753010e0466c
Sally Mclean <smclean17@gmail.com>
2020-05-31 15:25:54 +0200

    add orientation warning for mobiles

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -13,22 +13,65 @@ class TitleScene extends Phaser.Scene {
       this.scale.off('orientationchange')
       this.scale.on('orientationchange', (orientation) => {
         if (orientation === Phaser.Scale.PORTRAIT) {
-          if (this.scale.isFullscreen) { this.scale.stopFullscreen() }
+         if (this.scale.isFullscreen) {
+            this.scale.stopFullscreen()
+          }
+          if (!this.sys.game.device.os.desktop) {
+            this.loadOrientationWarning()
+          }
         } else if (orientation === Phaser.Scale.LANDSCAPE) {
+           if (!this.sys.game.device.os.desktop) {
+            this.loadStartButtons()
+          }
           if (!this.scale.isFullscreen) { this.scale.startFullscreen() }
         }
       })
-
-
 
       this.title = this.add.sprite(this.sys.game.config.width / 2, 16 * 5);
       this.scene.bringToTop();
 
       this.registry.set('restartScene', false);
 
-      let inputType = this.registry.get('inputType')
+      this.inputType = this.registry.get('inputType')
 
-      if (!inputType){
+      console.log(this.sys.game.device.os.desktop)
+      console.log(this.scale.orientation)
+      if (!this.sys.game.device.os.desktop && this.scale.orientation === Phaser.Scale.PORTRAIT) {
+        this.loadOrientationWarning()
+      } else
+      {
+        this.loadStartButtons()
+      }
+      this.add.text(500, 620, 'Version 1.0', { fill: "#00ff00", fontSize: 15 })
+    }
+
+    loadOrientationWarning() {
+      this.destroyOrientationWarning()
+      this.destroyStartButtons()
+      this.OrientationWarningText = this.add.text(100, 100, 'Switch your device to Landscape', { fill: "#F0091F", fontSize: 60 })
+      this.OrientationWarningText2 = this.add.text(100, 300, 'If the screen does not rotate:', { fill: "#00ff00", fontSize: 40 })
+      this.OrientationWarningText3 = this.add.text(100, 400, 'check Auto-rotate screen option is on (Android)', { fill: "#00ff00", fontSize: 40 })
+      this.OrientationWarningText4 = this.add.text(100, 500, 'Or Orientation Lock is off on Iphone', { fill: "#00ff00", fontSize: 40 })
+    }
+
+    destroyOrientationWarning() {
+      if (this.OrientationWarningText) {this.OrientationWarningText.destroy()}
+      if (this.OrientationWarningText2) {this.OrientationWarningText2.destroy()}
+      if (this.OrientationWarningText3) {this.OrientationWarningText3.destroy()}
+      if (this.OrientationWarningText4) {this.OrientationWarningText4.destroy()}
+
+    }
+
+    destroyStartButtons() {
+      if (this.pressStartMob) {this.pressStartMob.destroy()}
+      if (this.pressStart) {this.pressStart.destroy()}
+      if (this.pressStartVoice) {this.pressStartVoice.destroy()}
+    }
+
+    loadStartButtons() {
+      this.destroyStartButtons()
+      this.destroyOrientationWarning()
+      if (!this.inputType){
 
         this.pressStartMob = new InputButton(this, 200, 100, "START (mobile input)", { fill: "#4ceaee", fontSize: 60 }, this.startGameMobile, this)
         this.add.existing(this.pressStartMob)
@@ -44,26 +87,32 @@ class TitleScene extends Phaser.Scene {
         this.add.existing(this.pressStart)
       }
 
-        this.add.text(500, 620, 'Version 1.0', { fill: "#00ff00", fontSize: 15 })
     }
 
     update(time, delta) {
     }
 
+    checkFullScreenSwitch() {
+      if (!this.sys.game.device.os.desktop  && !this.scale.isFullscreen) { this.scale.startFullscreen() }
+    }
+
     startGamePreset() {
-      let inputType = this.registry.get('inputType')
-      this.scene.start('GameScene', {inputType: inputType});
+      this.checkFullScreenSwitch()
+      this.scene.start('GameScene', {inputType: this.inputType});
     }
 
     startGameMobile() {
+      this.checkFullScreenSwitch()
       this.scene.start('GameScene', {inputType: 'Touch'})
     }
 
     startGameVoice() {
+      this.checkFullScreenSwitch()
       this.scene.start('GameScene', {inputType: 'Voice'})
     }
 
     startGame() {
+      this.checkFullScreenSwitch()
       this.scene.start('GameScene', {inputType: 'Keyboard'});
     }
 }
